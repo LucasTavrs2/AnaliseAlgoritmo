e699d1f046b9b406d79766d1b9299543935c179e
Lucas Tavares <115112080+LucasTavrs2@users.noreply.github.com>
2025-08-17 23:04:45 -0300

    Add files via upload

---
+++ estudoAlgoritmo/quickSort.py
@@ -0,0 +1,17 @@
+def quicksort(array):
+
+  if len(array) < 2:
+
+    return array 
+
+  else:
+
+    pivo = array[0] 
+
+    menores = [i for i in array[1:] if i <= pivo]
+
+    maiores = [i for i in array[1:] if i > pivo]
+
+    return quicksort(menores) + [pivo] + quicksort(maiores)
+
+print(quicksort([10, 5, 2, 3, 7, 12, 14, 21, 4, 6]))
